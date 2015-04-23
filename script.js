a6ce9f6d0034010dda9ddbac407611b31b46a796
Shannon Smith <shannones2013@gmail.com>
2015-04-22 17:01:33 -0700

    refactor

---
+++ script.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+	var input = +`${prompt("Pick a number")}`;
+	for (var i=1; i <= input; i++) {
+		if (i % 15 == 0) {
+			$(body).append("fizzbuzz");
+		} else if (i % 3 == 0) {
+			$(body).append("fizz");
+		} else if (i % 5 == 0) {
+			$(body).append("buzz");
+		} else {
+			$(body).append(i);
+		}
+		$(body).append("<br>");
+  	}
+});
